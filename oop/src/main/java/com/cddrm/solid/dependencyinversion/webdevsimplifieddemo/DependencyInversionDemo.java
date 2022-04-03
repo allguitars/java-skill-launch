d98b861c0c414eef20a42ef48fc6c8300796a519
Dave Lee <isho.lee@gmail.com>
2022-04-03 22:21:25 +0800

    use separate objects for different paymentProcessors

--- oop/src/main/java/com/cddrm/solid/dependencyinversion/webdevsimplifieddemo/DependencyInversionDemo.java
+++ oop/src/main/java/com/cddrm/solid/dependencyinversion/webdevsimplifieddemo/DependencyInversionDemo.java
@@ -13,23 +13,23 @@ public class DependencyInversionDemo {
         User user = new User("dave", "dave@cddrm.com");
 
         // using Stripe
-        PaymentProcessor processor = new StripePaymentProcessor(user);
+        PaymentProcessor processor1 = new StripePaymentProcessor(user);
 
-        purchaseBike(processor, 2);
-        purchaseHelmet(processor, 2);
+        purchaseBike(processor1, 2);
+        purchaseHelmet(processor1, 2);
 
         // using PayPal
-        processor = new PayPalPaymentProcessor(user);
+        PaymentProcessor processor2 = new PayPalPaymentProcessor(user);
 
-        purchaseBike(processor, 2);
-        purchaseHelmet(processor, 2);
+        purchaseBike(processor2, 2);
+        purchaseHelmet(processor2, 2);
     }
 
     // *** Code to the interface!!
     // The PaymentProcessor interface provides a unified method and signature.
     // It allows the caller to keep the same way of using the payment API.
 
-    private static void purchaseBike(PaymentProcessor  paymentProcessor, int quantity) {
+    private static void purchaseBike(PaymentProcessor paymentProcessor, int quantity) {
         // the same method and argument
         paymentProcessor.pay(200 * quantity);
     }
