7d2bcff0e03ff37f5f229742196c25e0f5b902be
Dave Lee <isho.lee@gmail.com>
2022-04-07 01:42:08 +0800

    make the parent class abstract

--- oop/src/main/java/com/cddrm/solid/openclosed/web_dev_simplified_demo/good/question/Question.java
+++ oop/src/main/java/com/cddrm/solid/openclosed/web_dev_simplified_demo/good/question/Question.java
@@ -1,6 +1,6 @@
 package com.cddrm.solid.openclosed.web_dev_simplified_demo.good.question;
 
-public class Question {
+abstract public class Question {
 
     private String description;
     private String[] options;
@@ -15,9 +15,7 @@ public class Question {
         this.options = options;
     }
 
-    public void printQuestionChoices() {
-        // placeholder for the child class's override
-    }
+    abstract public void printQuestionChoices();
 
     public String[] getOptions() {
         return options;
