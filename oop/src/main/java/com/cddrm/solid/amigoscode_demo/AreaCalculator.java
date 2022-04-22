40221014ceabc728e55c0269ded6e002356f9cfd
Dave Lee <isho.lee@gmail.com>
2022-04-22 21:09:19 +0800

    add project pre-demo code for amigoscode's SOLID principle demo

---
+++ oop/src/main/java/com/cddrm/solid/amigoscode_demo/AreaCalculator.java
@@ -0,0 +1,20 @@
+package com.cddrm.solid.amigoscode_demo;
+
+import java.util.List;
+
+public class AreaCalculator {
+
+    public int sum(List<Object> shapes) {
+        int sum = 0;
+
+        for (Object shape : shapes) {
+            if (shape instanceof Square) {
+                sum += Math.pow(((Square) shape).getLength(), 2);
+            }
+            if (shape instanceof Circle) {
+                sum += Math.PI * Math.pow(((Circle) shape).getRadius(), 2);
+            }
+        }
+        return sum;
+    }
+}
---
+++ oop/src/main/java/com/cddrm/solid/amigoscode_demo/Square.java
@@ -0,0 +1,13 @@
+package com.cddrm.solid.amigoscode_demo;
+
+public class Square {
+    private final int length;
+
+    public Square(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
